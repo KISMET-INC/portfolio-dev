1ebf507954a1c81eda1f080ce1d7b3a422479e81
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-11-27 11:04:06 -0800

    Updated pictures

--- src/assets/js/my.js
+++ src/assets/js/my.js
@@ -1,5 +1,36 @@
-var name = "Kristesss"
-var d = document.getElementById("this");
+$(".hide").hide();
 
+$(document).on("click",".click-tech",function() {
+    if ($(this).closest(".tech").length ===0) { 
+        $(".hide").hide();
+    } 
 
-console.log(d)
+    $(".dropdown-tech").slideDown("fast");
+
+});
+$(document).on("click",".click-challenge",function() {
+    if ($(this).closest(".dropdown-challenge").length ===0) { 
+        $(".hide").hide();
+    } 
+    $(".dropdown-challenge").slideDown("fast")
+});
+
+$(document).on("click",".click-different",function() {
+    if ($(this).closest(".dropdown-different").length ===0) { 
+        $(".hide").hide();
+    } 
+    $(".dropdown-different").slideDown("fast")
+});
+$(document).on("click",".click-learn",function() {
+    if ($(this).closest(".dropdown-learn").length ===0) { 
+        $(".hide").hide();
+    } 
+    $(".dropdown-learn").slideDown("fast")
+});
+
+$(document).on("click",".click-plans",function() {
+    if ($(this).closest(".dropdown-plans").length ===0) { 
+        $(".hide").hide();
+    } 
+    $(".dropdown-plans").slideDown("fast")
+});
