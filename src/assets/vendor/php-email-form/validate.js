dd5e0596ca080bd6681c09ee6280c29eccd291c4
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-11-30 18:31:02 -0800

    update validate.js

--- src/assets/vendor/php-email-form/validate.js
+++ src/assets/vendor/php-email-form/validate.js
@@ -124,7 +124,26 @@
   });
 
   function php_email_form_submit(this_form, action, data) {
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: data,
+      timeout: 40000
+    }).done( function(msg){
+      if (msg.trim() == 'OK') {
+        this_form.find('.loading').slideUp();
+        this_form.find('.sent-message').slideDown();
+        this_form.find("input:not(input[type=submit]), textarea").val('');
+      } else {
+        this_form.find('.loading').slideUp();
+        if(!msg) {
+          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+        }
+        this_form.find('.error-message').slideDown().html(msg);
+      }
+    }).fail( function(data){
       console.log(data);
+      console.log(action);
       // var error_msg = "Form submission failed!<br>";
       // if(data.statusText || data.status) {
       //  error_msg += 'Status:';
@@ -142,5 +161,7 @@
       this_form.find('.loading').slideUp();
       this_form.find('.sent-message').slideDown();
       this_form.find("input:not(input[type=submit]), textarea").val('');
-    }
+    });
+  }
+
 })(jQuery);
