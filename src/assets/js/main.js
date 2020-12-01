d7b29b3014716aa9fd55d39f83906613550fb082
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-12-01 02:10:19 -0800

    cleaning up code 1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -107,19 +107,6 @@
     }, 1500, 'easeInOutExpo');
     return false;
   });
-
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, { offset: '80%'} );
   
   // Porfolio isotope and filter
   $(window).on('load', function () {
@@ -140,14 +127,6 @@
     $(document).ready(function(){
       $('.venobox').venobox(); 
     });
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 3 } }
   });
 
   // Portfolio details carousel
